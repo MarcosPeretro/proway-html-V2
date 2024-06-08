67b43543e886e2d24253b2721edd7117dfc70fd5
Marcos Vinicius Peretro Júnior <74966253+MarcosPeretro@users.noreply.github.com>
2024-06-07 22:15:00 -0300

    Add files via upload

---
+++ proway-html-V2-main/AULA CSS/js/main.js
@@ -0,0 +1,7 @@
+let nav = document.querySelector("nav")
+
+document.querySelector(".menu")
+.addEventListener("click", () => {
+    let nav = document.querySelector("nav")
+    nav.classList.toggle("hamburguer")
+})
